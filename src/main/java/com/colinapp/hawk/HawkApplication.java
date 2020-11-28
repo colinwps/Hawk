039ab1a35282f48a35d0b64c0ffa6e7caf67ffcb
admin <admin@ADMIN-PC>
2020-11-28 15:01:16 +0800

    Initial commit

---
+++ src/main/java/com/colinapp/hawk/HawkApplication.java
@@ -0,0 +1,13 @@
+package com.colinapp.hawk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HawkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colinapp/hawk/HawkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colinapp.hawk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
