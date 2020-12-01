371601258e063b4abd2a340f954dfa8f8f7b8cdb
admin <admin@ADMIN-PC>
2020-12-01 15:32:45 +0800

    mysql

--- src/test/java/com/colinapp/hawk/HawkApplicationTests.java
+++ src/test/java/com/colinapp/hawk/HawkApplicationTests.java
@@ -1,13 +1,21 @@
 package com.colinapp.hawk;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class HawkApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        String sql = (String) jdbcTemplate.queryForObject("SELECT userName FROM sys_user WHERE userId= ?",new Object[]{"8001"},String.class);
+        System.out.println(sql);
+
     }
 
 }
